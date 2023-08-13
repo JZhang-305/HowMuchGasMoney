4343523341b34452a16ef5deb23016b75a400632
Jonathan Zhang <54372027+JZhang-305@users.noreply.github.com>
2023-08-13 17:27:11 -0400

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -200,7 +200,7 @@ def calculate_distance(api_key, origin, destination, mpg, costOfGas, roundtrip=F
 # Variables for user inputs
 st.title("How Much For ⛽")
 st.header("HowMuchForGas.com")
-st.text("The best gas price calculator for extorting your cheap friends! Or you can use this to calculate gas from A to B with no pickups but what's the fun in that!")
+st.write("The best gas price calculator for extorting your cheap friends! Or you can use this to calculate gas from A to B with no pickups but what's the fun in that!")
 #origin = st.text_input("Origin Address 🚩", value = "12 Poplar Lane, Commack NY 11725", placeholder="where u start from")
 origin = st.text_input("Origin Address 🚩", placeholder="where u start from")
 #destination = st.text_input("Destination Address 💹", value = "2020 Jericho Tpke, Commack NY 11725", placeholder = "where u going")

405dd66080c2469b6b7a323663aa062a60bf5abc
Jonathan Zhang <54372027+JZhang-305@users.noreply.github.com>
2023-10-16 23:08:26 -0400

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -309,12 +309,12 @@ if len(origin) != 0 and len(destination) != 0 and len(str(mpg)) != 0 and len(str
             st.write("I saved **$" + str(results['total_extra_cost'] * -1) + "** in gas money 💹")
         
         '''--------------------------------------------'''
-        st.write("I'm a W UI designer - jz 7/28/2023")
-        st.write("ibrahim owes me a lot of money")
-        st.write("coproduced by jordan vogel")
+        # st.write("I'm a W UI designer - jz 7/28/2023")
+        # st.write("ibrahim owes me a lot of money")
+        # st.write("coproduced by jordan vogel")
         #st.write(results['list_of_addresses'])
     
     else:
         # if a route could not be determined
-        st.write("your inputs are wack, no route could be found")
+        st.write("your inputs are wacky, no route could be found")
     
